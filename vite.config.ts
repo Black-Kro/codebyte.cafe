8447cca3a2deeab1791833c7f41466789f18c220
Jacob Clevenger <jacobrclevenger@gmail.com>
2020-08-30 22:11:40 -0400

    removed unused stuff from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -18,29 +18,3 @@ const config: UserConfig = {
 }
 
 export default config;
-
-
-// import { UserConfig } from 'vite';
-// import ViteComponents from 'vite-plugin-components';
-// const path = require('path');
-
-// const alias = {
-//     '/~/': path.resolve(__dirname, 'src'),
-//     '/@app/composables/': path.resolve(__dirname, './src/shared/composables'),
-//     '/@app/components/': path.resolve(__dirname, './src/shared/components'),
-//     '/@app/services/': path.resolve(__dirname, './src/shared/services'),
-//     '/@app/styles/': path.resolve(__dirname, './src/shared/styles'),
-//     '/@app/store/': path.resolve(__dirname, './src/shared/store'),
-//     '/@app/router/': path.resolve(__dirname, './src/shared/router'),
-//     '/@app/gql/': path.resolve(__dirname, './src/shared/gql'),
-//     '/@app/': path.resolve(__dirname, './src'),
-// }
-
-// module.exports = {
-//     alias,
-//     plugins: [
-//         ViteComponents({
-//             alias
-//         })
-//     ]
-// } as UserConfig
