5735f9fb5edbe3656ab82d29c607f2908521cf58
Jacob Clevenger <jacobrclevenger@gmail.com>
2020-08-31 21:49:37 -0400

    Expose ws env var

---
+++ src/components/AppNotification.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Notifciation
+    </div>
+</template>
+
+<script lang="ts" setup>
+    export default {
+        name: '',
+    }
+</script>
+
+<style lang="scss">
+    
+</style>
